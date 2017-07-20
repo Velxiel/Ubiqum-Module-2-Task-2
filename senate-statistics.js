8b115a6bcedc8026d46b0e69fe582260de6ff8f0
Harry MAkrinos <velxiel@gmail.com>
2017-07-20 11:11:24 +0200

    Finished Task 4

---
+++ senate-statistics.js
@@ -0,0 +1,23 @@
+// A var named 'statistics' is created
+// It contains an object, that can be used for the calculations later
+var statistics = {
+  no_of_reps: {
+    "Number_of_Republicans": 0,
+    "Number_of_Democrats": 0,
+    "Number_of_Independents": 0,
+    "Total": 0
+  },
+  percentage_of_voted_parties: {
+    "Pecentage_of_Republicans": 0,
+    "Pecentage_of_Democrats": 0,
+    "Pecentage_of_Independents": 0,
+    "Pecentage_of_Total": 0 + "%"
+  },
+  no_of_missed_votes: {
+    "Missed_Votes_of_Republicans": 0,
+    "Missed_Votes_of_Democrats": 0,
+    "Missed_Votes_of_Independents": 0,
+    "Total": 0
+  }
+
+};
